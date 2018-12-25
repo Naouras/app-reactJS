80a22c48a229f9ee1e5cbdd9a96e10079081637d
Naoures Hadj Amor <naoures.hadjamor@ett.tn>
2018-12-25 18:04:49 +0100

    start with API

--- fem/src/app.js
+++ fem/src/app.js
@@ -1,26 +1,53 @@
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./pet"
+import pf from "petfinder-client";
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
-  ]);
-};
+  // under imports
+  const petfinder = pf({
+    key: process.env.API_KEY,
+    secret: process.env.API_SECRET
+  });
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      pets: []
+    };
+  }
+  // replace cDM
+  componentDidMount() {
+    petfinder.pet
+      .find({ location: "Seattle, WA", output: "full" })
+      .then(data => {
+        let pets;
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = []
+        }
+        this.setState({
+          pets
+        });
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        <Pet name="Luna" animal="dog" breed="Havanese" />
+        // inside render, under h1
+       <pre>
+         <code>{JSON.stringify(this.state, null, 2)}</code>
+       </pre>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
